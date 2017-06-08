550660291eabf280e2fbee7b2411351e26cb5f58
Rathi <github email address>
2017-06-08 15:56:38 -0400

    included bg

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,10 +1,18 @@
-function buttonClick(){
- var value = document.getElementById('item').value;
- if(value) {
-    addItemToList(value)
-  value = document.getElementById('item').value = '' ;
+document.getElementById('addItem').addEventListener('click', buttonClick);
 
- }
+document.getElementById('item').addEventListener("keyup", function(event) {
+  event.preventDefault();
+  if (event.keyCode == 13) {
+    document.getElementById('addItem').click();
+  }
+});
+
+function buttonClick(){
+  var value = document.getElementById('item').value;
+  if(value) {
+    addItemToList(value)
+    value = document.getElementById('item').value = '' ;
+  }
 }
 
 function removeItem(){
@@ -17,7 +25,7 @@ function completeItem(){
   var item = this.parentNode.parentNode;
   var parent = item.parentNode;
   var id = parent.id;
-  
+
   var target = (id ==='list') ? document.getElementById("completed") : document.getElementById('list')
 
   parent.removeChild(item)
@@ -27,27 +35,24 @@ function completeItem(){
 
 function addItemToList(item){
   var lis = document.getElementById('list');
-   var el = document.createElement('li');
-   el.innerText = item ;
+  var el = document.createElement('li');
+  el.innerText = item ;
 
-   var buttons = document.createElement('div');
-   buttons.classList.add('buttons');
+  var buttons = document.createElement('div');
+  buttons.classList.add('buttons');
 
-   var remove = document.createElement('button');
-   remove.classList.add('remove')
-   remove.innerHTML = "delete"
-   remove.addEventListener('click', removeItem);
+  var remove = document.createElement('button');
+  remove.classList.add('remove')
+  remove.innerHTML = "delete"
+  remove.addEventListener('click', removeItem);
 
-   var complete = document.createElement('button');
-   complete.classList.add('complete')
-   complete.innerHTML = "done"
-   complete.addEventListener('click', completeItem);
+  var complete = document.createElement('button');
+  complete.classList.add('complete')
+  complete.innerHTML = "done"
+  complete.addEventListener('click', completeItem);
 
-   buttons.appendChild(remove);
-   buttons.appendChild(complete);
-   el.appendChild(buttons);
-   lis.insertBefore(el, list.childNodes[0]);
+  buttons.appendChild(remove);
+  buttons.appendChild(complete);
+  el.appendChild(buttons);
+  lis.insertBefore(el, list.childNodes[0]);
 }
-
-
-document.getElementById('addItem').addEventListener('click', buttonClick);
